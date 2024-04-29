1fd5b487d56e6a2dfa35d60d669671bad961463b
pallavi <pmdhakne2002@gmail.com>
2024-04-29 20:26:52 +0530

    chatmodel created

---
+++ backend/model/chatModel.js
@@ -0,0 +1,35 @@
+//chatname
+//isgroupchat
+//users
+//lastetmessage
+//groupadmin
+
+import mongoose from mongoose
+const chatModel=mongoose.Schema(
+    {
+        chatName:{
+            type:String,
+            trim:true,
+            Required:true
+        },
+        isgroupchat:{
+            type:Boolean,
+            default:false
+        },
+        users:[
+            {
+                type:mongoose.Schema.Type.ObjectId,
+                ref:"User",
+            },
+        ],
+        latestMessage:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Message",
+        },
+        groupAdmin:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+        },
+},{
+    timestamps:true,
+});
