02cc6948b58f289337addc58217d93403e73bc55
pallavi <pmdhakne2002@gmail.com>
2024-05-07 17:12:49 +0530

    created controller and web token adding is pending

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../model/userModel");
-const registerUser = asyncHandler(async () => {
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
 
   if (!name || !email || !password) {
@@ -8,17 +8,31 @@ const registerUser = asyncHandler(async () => {
     throw new Error("Please Enter all the feilds");
   }
 
-  const userExists = await User.findone({ email });
+  const userExists = await User.findOne({ email });
   if (userExists) {
     res.status(400);
-    throw new error("User Already Exists Please Login in");
+    throw new Error("User Already Exists Please Login in");
   }
   const user = await User.create({
     name,
     email,
     password,
     pic,
+    token: generateToken(user._id),
   });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed to create new user");
+  }
 });
 
-module.export = registerUser;
+module.exports = { registerUser };
